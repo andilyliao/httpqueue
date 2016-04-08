5111effdf0602f28b1cc9d6a0ce7465820c6b9ac
andilyliao <andilyliao@126.com>
2016-04-08 12:37:31 +0800

    add

--- httpqueue/src/main/java/org/httpqueue/outprocess/task/ReadMemory.java
+++ httpqueue/src/main/java/org/httpqueue/outprocess/task/ReadMemory.java
@@ -17,14 +17,12 @@ public class ReadMemory implements IReadMemory {
     @Override
     public void registDirectQueue(String queueName) throws Exception {
         //TODO 目前没有任务，后面需要做每个队列的注册消费者数量的监控
-        //TODO regist需要返回当前的pubset，以及offset
         //TODO 如果注册过需要处理注册过的逻辑
     }
 
     @Override
     public int registFanoutQueue(String clientID,String queueName) throws Exception {
         //TODO 后面需要做每个队列的注册消费者数量的监控
-        //TODO regist需要返回当前的pubset，offset=0
         //TODO 如果注册过需要处理注册过的逻辑
         ShardedJedis jedis = RedisShard.getJedisObject();
         String pubset="0";
@@ -42,7 +40,6 @@ public class ReadMemory implements IReadMemory {
     @Override
     public int registTopic(String clientID,String queueName) throws Exception {
         //TODO 后面需要做每个队列的注册消费者数量的监控
-        //TODO regist需要返回当前的pubset，虽然返回offset，但是offset=pubset
         //TODO 如果注册过需要处理注册过的逻辑
         ShardedJedis jedis=RedisShard.getJedisObject();
         String pubset="0";
