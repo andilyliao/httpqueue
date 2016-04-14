ab1f344b5a55142ffacffc8dd350caab62cc5265
andilyliao <andilyliao@126.com>
2016-04-14 15:26:23 +0800

    add

--- httpqueue/src/main/java/org/client/publisher/PersistDirectQueuePublisher.java
+++ httpqueue/src/main/java/org/client/publisher/PersistDirectQueuePublisher.java
@@ -1,17 +1,23 @@
 package org.client.publisher;
 
 
+import com.alibaba.fastjson.JSON;
+import org.apache.log4j.Logger;
 import org.client.publisher.intf.IPublisher;
+import org.client.publisher.intf.Publish;
 import org.client.publisher.util.config.Config;
 import org.client.publisher.util.messageconfig.Message;
+import org.client.publisher.util.queueconfig.PersistDirectQueueConfig;
 import org.client.publisher.util.result.CommonRes;
 
 /**
  * Created by andilyliao on 16-4-2.
  */
 //
-public class PersistDirectQueuePublisher implements IPublisher {
+public class PersistDirectQueuePublisher extends Publish implements IPublisher {
+    private static Logger log = Logger.getLogger(MemoryDirectQueuePublisher.class);
     private Config config;
+    private PersistDirectQueueConfig queueConfig;
     public PersistDirectQueuePublisher(Config config){
         this.config=config;
     }
@@ -21,10 +27,28 @@ public class PersistDirectQueuePublisher implements IPublisher {
         this.config=config;
     }
     //curl http://localhost:8844/queue -d '{"head":{"qn":"mydirectqueue","ty":0,"m":0,"t":86400,"h":0}}'
-
+    public CommonRes createDirectQueue(PersistDirectQueueConfig queueConfig) throws Exception {
+        this.queueConfig=queueConfig;
+        String queueName = queueConfig.getQueueName();
+        int ttl = queueConfig.getTtl();
+        String url = this.config.urlMap.get(Math.abs(queueName.hashCode()) % this.config.urlMap.size());
+        String json = "{\"head\":{\"qn\":\""+queueName+"\",\"ty\":0,\"m\":0,\"t\":"+ttl+",\"h\":1}}";
+        String body = send(url, json, "utf-8");
+        CommonRes cr= JSON.parseObject(body, CommonRes.class);
+        return cr;
+    }
 //curl http://localhost:8844/queue -d '{"head":{"qn":"mydirectqueue","ty":1,"h":0,"tr":0,"s":0,"ts":0},"body":{"aaa":"bbb","ccc":"ddd"}}'
     @Override
     public CommonRes publishMessage(Message message) throws Exception {
-        return new CommonRes();
+        String queueName = this.queueConfig.getQueueName();
+        String msg=message.getBody();
+        String url = this.config.urlMap.get(Math.abs(queueName.hashCode()) % this.config.urlMap.size());
+        String json = "{\"head\":{\"qn\":\""+queueName+"\",\"ty\":1,\"h\":1,\"tr\":0,\"s\":0,\"ts\":0},\"body\":\""+msg+"\"}";
+        String body = send(url, json, "utf-8");
+        CommonRes cr=JSON.parseObject(body, CommonRes.class);
+        return cr;
+    }
+    public void initQueueWithoutCreate(PersistDirectQueueConfig queueConfig)throws Exception {
+        this.queueConfig=queueConfig;
     }
 }
--- httpqueue/src/main/java/org/client/publisher/util/queueconfig/PersistDirectQueueConfig.java
+++ httpqueue/src/main/java/org/client/publisher/util/queueconfig/PersistDirectQueueConfig.java
@@ -4,4 +4,24 @@ package org.client.publisher.util.queueconfig;
  * Created by andilyliao on 16-4-6.
  */
 public class PersistDirectQueueConfig {
+    //"qn":"mydirectqueue","t":86400
+    private String queueName="";
+    private int ttl=0;
+    public static final int ONE_DAY=24*3600;
+
+    public String getQueueName() {
+        return queueName;
+    }
+
+    public void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(int ttl) {
+        this.ttl = ttl;
+    }
 }
